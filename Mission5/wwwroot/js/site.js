ed1c641fc221b9415b1d30305672b873427cfc7f
tracewilson <wiltrac3@byu.edu>
2025-03-03 13:59:19 -0700

    missed commit

--- Mission5/wwwroot/js/site.js
+++ Mission5/wwwroot/js/site.js
@@ -1,4 +1,19 @@
 ﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
 // for details on configuring this project to bundle and minify static web assets.
 
-// Write your JavaScript code.
+$("#btnSubmit").click(function(event) {
+    event.preventDefault(); // Prevent form submission
+
+    let hours = parseInt($("#numHours").val(), 10);
+    let rate = 40;
+
+    if (isNaN(hours) || hours < 0) {
+        hours = 0;
+    } else if (hours > 50) {
+        hours = 50;
+    }
+
+    let estimate = hours * rate;
+
+    $("#estimatedCost").val(estimate);
+});
